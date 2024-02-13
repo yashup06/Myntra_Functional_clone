49c204c7bb080aa8894351243f0b40dbbfd347fd
yashup06 <yashupadhyay333@gmail.com>
2024-02-14 00:37:37 +0530

    added all files

---
+++ scripts/bag.js
@@ -0,0 +1,111 @@
+let bagItemObjects;
+
+
+onLoad();
+
+function onLoad(){
+  displayBagIcon();
+  loadBagItems();
+  displayBagItem();
+  displayBagSummary();
+  
+}
+
+function displayBagSummary(){
+  let bagSummaryElement = document.querySelector('.bag-summary');
+
+  let totalItem = bagItems.length;
+  let totalMRP = 0;
+  let totalDiscount = 0;
+  let finalPayment = 0;
+
+  bagItemObjects.forEach(bagItem =>  {
+    totalMRP += bagItem.original_price;
+    totalDiscount += bagItem.discount_percentage;
+  })
+  finalPayment = totalMRP - totalDiscount + 99 ;
+
+    bagSummaryElement.innerHTML = `
+    <div class="bag-details-container">
+    <div class="price-header">PRICE DETAILS (${totalItem}Items) </div>
+    <div class="price-item">
+      <span class="price-item-tag">Total MRP</span>
+      <span class="price-item-value">Rs ${totalMRP}</span>
+    </div>
+    <div class="price-item">
+      <span class="price-item-tag">Discount on MRP</span>
+      <span class="price-item-value priceDetail-base-discount">-Rs ${totalDiscount}</span>
+    </div>
+    <div class="price-item">
+      <span class="price-item-tag">Convenience Fee</span>
+      <span class="price-item-value">Rs 99</span>
+    </div>
+    <hr>
+    <div class="price-footer">
+      <span class="price-item-tag">Total Amount</span>
+      <span class="price-item-value">Rs ${finalPayment}</span>
+    </div>
+  </div>
+  <button class="btn-place-order">
+    <div class="css-xjhrni">PLACE ORDER</div>
+  </button>`
+}
+
+function removeFromBag(itemId){
+  bagItems=bagItems.filter(bagItemId => bagItemId != itemId );
+  localStorage.setItem('bagItems',JSON.stringify(bagItems));
+  onLoad();
+}
+
+function loadBagItems(){
+  
+  bagItemObjects = bagItems.map(itemId => {
+    for(let i = 0; i< items.length ; i++){
+      if(itemId == items[i].id){
+        return items[i];
+      }
+    } 
+    
+   });
+   console.log(bagItemObjects);
+}
+
+function displayBagItem(){
+  
+
+  let bagContainerElement = document.querySelector('.bag-items-container');
+  let innerHTML = '';
+  bagItemObjects.forEach(bagItem => {
+    innerHTML += generateItemHtml(bagItem);
+    
+  });
+   bagContainerElement.innerHTML = innerHTML;
+  
+}
+
+
+function generateItemHtml(item){
+  return `<div class="item-left-part">
+  <img class="bag-item-img" src="../${item.image}">
+</div>
+<div class="item-right-part">
+  <div class="company">${item.company}</div>
+  <div class="item-name">${item.item_name}</div>
+  <div class="price-container">
+    <span class="current-price">Rs ${item.current_price}</span>
+    <span class="original-price">Rs ${item.original_price}</span>
+    <span class="discount-percentage">(${item.discount_percentage}% OFF)</span>
+  </div>
+  <div class="return-period">
+    <span class="return-period-days">${item.return_period} days</span> return available
+  </div>
+  <div class="delivery-details">
+    Delivery by
+    <span class="delivery-details-days">${item.delivery_date}</span>
+  </div>
+  <div class="remove-from-cart" onclick="removeFromBag(${item.id});">X</div>
+
+</div>`
+
+
+}
